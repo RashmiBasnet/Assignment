041e096fa6714e250d72ea9497eb9c2bf1593803
RashmiBasnet <rashmibasnet2019@gmail.com>
2024-11-20 22:42:59 +0545

    assignment

---
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.assignment
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var fn: TextInputLayout
+    lateinit var ln: TextInputLayout
+    lateinit var email: TextInputLayout
+    lateinit var pw: TextInputLayout
+    lateinit var btn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        fn = findViewById(R.id.firstName)
+        ln = findViewById(R.id.lastName)
+        email = findViewById(R.id.Email)
+        pw = findViewById(R.id.pw)
+        btn = findViewById(R.id.signButton)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
